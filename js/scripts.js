9fea5bb034f303237c5e8028df4b29520bc070a0
Taksheel-ai <77297347+Taksheel-ai@users.noreply.github.com>
2022-01-31 19:37:14 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
     */
 
-setTimeout(() => {
-            alert('Slashed Prices! Web Build worth Rs. 400 now at ONLY Rs. 190!! Use the Facebook button to DM me!');
-        }, 10000);
+// setTimeout(() => {
+//             alert('Slashed Prices! Web Build worth Rs. 400 now at ONLY Rs. 190!! Use the Facebook button to DM me!');
+//         }, 10000);
        
     (function($) {
     "use strict"; // Start of use strict
